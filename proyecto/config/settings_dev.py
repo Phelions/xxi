6ba91428415f93c8aeb4a6f83ac66ca8662ad3d1
León <56101523+Phelions@users.noreply.github.com>
2022-11-27 08:40:38 +0000

    Changes to be committed: 	modified:   proyecto/config/settings_dev.py

--- proyecto/config/settings_dev.py
+++ proyecto/config/settings_dev.py
@@ -28,4 +28,4 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR ,'src/static') 
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "STATIC_URL")
