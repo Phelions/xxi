18f50b9b209d4985e3db6ebe838e4e001e414c93
León <56101523+Phelions@users.noreply.github.com>
2022-11-28 18:36:08 +0000

    Changes to be committed: 	new file:   proyecto/users/migrations/0002_auto_20221128_1835.py 	modified:   proyecto/users/views.py

--- proyecto/users/views.py
+++ proyecto/users/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
-from .models import Usuario
+# from .models import Usuario
 # Create your views here.
 
 
 def login(request):
 
-    if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
-
-        checklogin = Usuario.objects.filter(email=email, password=password).values()
-
-    else:
-        datos = {'r2' : 'No se puede procesar la solicitud'}
-    return render(request, 'accounts/login.html',datos)
+#    if request.method == 'POST':
+#        email = request.POST['email']
+#        password = request.POST['password']
+#        checklogin = Usuario.objects.filter(email=email, password=password).values()       
+#        if checklogin:
+#            print(checklogin[0])
+#        else:
+#            checklogin = {'r2' : 'Error de Usuario y/o Contraseña!!'}
+#            return render(request, 'accounts/login.html', checklogin)
+#    else:
+#        datos = {'r2' : 'No se puede procesar la solicitud'}
+    return render(request, 'accounts/login.html')
 
 def logout(request):
     return render(request, 'accounts/logout.html')
