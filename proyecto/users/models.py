48c6452479cf71b8921ec4eedf884ce94fc0afb7
León <56101523+Phelions@users.noreply.github.com>
2022-11-28 03:05:45 +0000

    Changes to be committed: 	modified:   proyecto/config/settings_caprover.py 	new file:   proyecto/users/migrations/0003_auto_20221128_0039.py 	new file:   proyecto/users/migrations/0004_auto_20221128_0304.py 	modified:   proyecto/users/models.py

--- proyecto/users/models.py
+++ proyecto/users/models.py
@@ -1,34 +1,37 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, MinLengthValidator, MaxLengthValidator
 
 # Create your models here.
 
 class Usuario(models.Model):
-    rut = models.PositiveIntegerField(default=9, validators=[MinValueValidator(9), MaxValueValidator(9)], primary_key=True)
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    celular = models.PositiveIntegerField(default=9, validators=[MinValueValidator(9), MaxValueValidator(9)])
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
+    rut = models.PositiveIntegerField(default=9, validators=[MinValueValidator(9), MaxValueValidator(9)], primary_key=True,null=False)
+    nombre = models.CharField(max_length=50,null=False)
+    apellido = models.CharField(max_length=50,null=False)
+    celular = models.PositiveIntegerField(default=9, validators=[MinValueValidator(9), MaxValueValidator(9)],null=False)
+    email = models.EmailField(max_length=100,null=False)
+    password = models.CharField(max_length=12,null=False)
 
     def __str__(self):
         return self.nombre
 
 class Historial(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    descripcion = models.CharField(max_length=255)
-    tabla_afectada = models.CharField(max_length=50)
-    fecha_hora = models.DateTimeField()
+    descripcion = models.CharField(max_length=255,null=False)
+    tabla_afectada = models.CharField(max_length=50,null=False)
+    fecha_hora = models.DateTimeField(null=False)
 
 class Turno(models.Model):
-    horario = models.CharField(max_length=255)
+    horario = models.CharField(max_length=255,null=False)
+
+class Tipo_empleado(models.Model):
+    nombre_rol = models.CharField(max_length=20,null=False)
 
 class Empleado(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    tipo_empleado = models.ForeignKey(Tipo_empleado, on_delete=models.CASCADE)
     turno = models.ForeignKey(Turno, on_delete=models.CASCADE)
-    tipo = models.CharField(max_length=50)
-    fecha_ingreso = models.DateField()
-    fecha_termino = models.DateField()
+    fecha_ingreso = models.DateField(null=False)
+    fecha_termino = models.DateField(null=False)
 
     def __str__(self):
         return self.usuario.nombre
