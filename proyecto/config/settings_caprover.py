e12a934d45d7fb0b1ae49d967af9d402d604a538
León <56101523+Phelions@users.noreply.github.com>
2022-11-28 19:43:37 +0000

    Changes to be committed: 	modified:   proyecto/config/settings_caprover.py 	modified:   proyecto/config/settings_dev.py

--- proyecto/config/settings_caprover.py
+++ proyecto/config/settings_caprover.py
@@ -20,9 +20,14 @@ except:
 # Database
 if os.environ.get("CR_USESQLITE"):
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME':'postgres',
+            'POSTGRES_DB': 'restaurant',
+            'USER': 'dbadmin',
+            'PASSWORD': 'dbpassword',
+            'HOST': 'postgres.restaurante-xxi.live',
+            'PORT': '5432',
         }
     }
 else:
--- proyecto/config/settings_dev.py
+++ proyecto/config/settings_dev.py
@@ -16,9 +16,14 @@ ALLOWED_HOSTS = ["*"]
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'postgres',
+        'POSTGRES_DB': 'restaurant',
+        'USER': 'dbadmin',
+        'PASSWORD': 'dbpassword',
+        'HOST': 'postgres.restaurante-xxi.live',
+        'PORT': '5432',
     }
 }
 
