bda7e0d9dc06e9e8a4f5371b95fbc50e0671cd70
León <56101523+Phelions@users.noreply.github.com>
2022-11-28 21:42:18 +0000

    Changes to be committed: 	modified:   proyecto/src/templates/accounts/login.html 	modified:   proyecto/users/views.py

--- proyecto/users/views.py
+++ proyecto/users/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
-# from .models import Usuario
+from manager.models import Usuario
 # Create your views here.
 
 
 def login(request):
 
-#    if request.method == 'POST':
-#        email = request.POST['email']
-#        password = request.POST['password']
-#        checklogin = Usuario.objects.filter(email=email, password=password).values()       
-#        if checklogin:
-#            print(checklogin[0])
-#        else:
-#            checklogin = {'r2' : 'Error de Usuario y/o Contraseña!!'}
-#            return render(request, 'accounts/login.html', checklogin)
-#    else:
-#        datos = {'r2' : 'No se puede procesar la solicitud'}
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        checklogin = Usuario.objects.filter(email=email, password=password).values()       
+        if checklogin:
+            request.session['estadoSesion'] = True
+            request.session['idUser'] = checklogin[0]['id']
+            request.session['email'] = checklogin[0]['email']
+            
+        else:
+            checklogin = {'r2' : 'Error de Usuario y/o Contraseña!!'}
+            return render(request, 'accounts/login.html', checklogin)
+    else:
+        datos = {'r2' : 'No se puede procesar la solicitud'}
     return render(request, 'accounts/login.html')
 
 def logout(request):
