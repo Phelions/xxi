5a5b71e394ca2ec7683f929a2e8f53d3ee5a0a64
phelions <phelions.dev@gmail.com>
2022-12-10 20:22:16 -0300

    fix

--- proyecto/manager/admin.py
+++ proyecto/manager/admin.py
@@ -1,7 +1,7 @@
-# from django.contrib import admin
-# from .models import Mesa, Reserva, EstadoMesa, Empleado
+from django.contrib import admin
+from .models import Mesa, Reserva, EstadoMesa, Empleado
 
-# admin.site.register(Mesa)
-# admin.site.register(Reserva)
-# admin.site.register(EstadoMesa)
-# admin.site.register(Empleado)
+admin.site.register(Mesa)
+admin.site.register(Reserva)
+admin.site.register(EstadoMesa)
+admin.site.register(Empleado)
