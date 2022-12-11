c80e7014fa9d491b32e1de422fe7854e4a56f6ee
phelions <phelions.dev@gmail.com>
2022-12-11 02:29:57 -0300

    fix

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-from .views import Register
-from django.shortcuts import render
-
-urlpatterns = [
-    path('accounts/',include('django.contrib.auth.urls')),
-    path('accounts/register/',Register.as_view(),name='register'),
-]
--- administrador/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-
-    path('dashboard/',views.dashboard,name='dashboard'),
-    path('mesa/',views.list_menu,name='list_menu'),
-
-]
